1f7420a125013941035ae3f85f458744b3e24206
ntauzovic <ntauzovic@gmail.com>
2025-12-18 14:55:39 +0100

    feat(custom-dropdown): create custom dropdown

---
+++ src/types/selectOptions.ts
@@ -0,0 +1,18 @@
+export const sortOptions = [
+  { label: "Default", value: "" },
+  { label: "Last name", value: "lastName" },
+  { label: "First name", value: "firstName" },
+  { label: "Email", value: "email" },
+];
+export const orderOptions = [
+  { label: "Default", value: "" },
+  { label: "ASC", value: "asc" },
+  { label: "DESC", value: "desc" },
+];
+
+export const rowsOptions = [
+  { label: "10", value: "10" },
+  { label: "20", value: "20" },
+  { label: "25", value: "25" },
+  { label: "50", value: "50" },
+];
