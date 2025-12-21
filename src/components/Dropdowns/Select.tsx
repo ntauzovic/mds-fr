6a970be2f02ecb9fd8f6d3641b50c963a8ab7387
ntauzovic <ntauzovic@gmail.com>
2025-12-21 17:33:49 +0100

    fix(select-close):  close dropdown on outside click

--- src/components/Dropdowns/Select.tsx
+++ src/components/Dropdowns/Select.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { Icon } from "../Icons/Icon";
 import type { SelectProps } from "../../types/selectOptions";
 
@@ -9,39 +9,37 @@ export function Select({
   onChange,
 }: SelectProps) {
   const [open, setOpen] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
 
-  const selected = options.find((o) => o.value === value);
+  const selected = options.find((o) => String(o.value) === String(value));
 
   return (
-    <div ref={ref} className="relative w-full">
+    <div tabIndex={0} onBlur={() => setOpen(false)} className="relative w-full">
       <button
         type="button"
         onClick={() => setOpen((v) => !v)}
         className="
-    relative
-    flex w-full items-center
-    rounded-lg border border-gray-300 bg-white
-    px-4 py-3 pr-10
-    text-sm text-gray-900
-    hover:border-gray-400
-    focus:outline-none focus:ring-2 focus:ring-red-500
-  "
+          relative flex w-full items-center
+          rounded-lg border border-gray-300 bg-white
+          px-4 py-3 pr-10
+          text-sm text-gray-900
+          hover:border-gray-400
+          focus:outline-none focus:ring-2 focus:ring-red-500
+        "
       >
-        <span className={selected ? "" : "text-blck"}>
+        <span className={selected ? "" : "text-gray-400"}>
           {selected?.label ?? placeholder}
         </span>
+
         <Icon
           name="angle-down"
           className="
     pointer-events-none
     absolute
-    right-2
+    right-0.5
     top-7.5
     h-5.5 w-5.5
     -translate-y-1/2
-    text-gray-600
-  "
+    text-gray-600"
         />
       </button>
 
@@ -57,6 +55,7 @@ export function Select({
           {options.map((option) => (
             <button
               key={option.value}
+              type="button"
               onClick={() => {
                 onChange(option.value);
                 setOpen(false);
@@ -65,7 +64,7 @@ export function Select({
                 flex w-full px-4 py-2 text-sm text-left
                 hover:bg-gray-100
                 ${
-                  option.value === value
+                  String(option.value) === String(value)
                     ? "bg-red-50 text-red-600 font-medium"
                     : "text-gray-700"
                 }
